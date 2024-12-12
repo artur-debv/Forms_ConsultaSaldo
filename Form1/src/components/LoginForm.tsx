8506069afdc07e63a1a989c9df5c1199d903cdbb
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-12-12 08:50:21 -0300

    refactor: I removed a div that was no longer being used in the form

--- Form1/src/components/LoginForm.tsx
+++ Form1/src/components/LoginForm.tsx
@@ -35,23 +35,12 @@ const LoginForm = () => {
                         <Button type="submit">Entrar</Button>
                     </div>
                 </form>
-
-                <div className="mt-6">
-                    <div className="relative">
-                        <div className="absolute inset-0 flex items-center">
-                            <div className="w-full border-t border-gray-300"></div>
-                        </div>
-                        <div className="relative flex justify-center text-sm">
-                            {/*<span className="px-2 bg-white text-gray-500">Ou continue com</span>*/}
-                        </div>
-                    </div>
-                    <div className="mt-6 grid grid-cols-1 justify-items-center items-center ">
-                        { /*<SocialButton href="#">
+                <div className="mt-6 grid grid-cols-1 justify-items-center items-center ">
+                    { /*<SocialButton href="#">
                             <span className="sr-only">Continue with Google</span>
                             <FaGoogle />
                             Google
                         </SocialButton>*/}
-                    </div>
                 </div>
             </div>
         </div>
