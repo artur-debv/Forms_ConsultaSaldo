14e1a51ca9396c36df352be2ba8843fb69a5f851
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-12-12 11:09:24 -0300

    feat: decreases the size of the header image, I called the header responsive using the md attribute in my main file the loginform

--- Form2/src/components/Header.jsx
+++ Form2/src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import logoJcsx from '../assets/logoJcsx.svg';
 
 export const Header = () => {
-  return (
-    <div className="hidden md:block fixed top-0 left-0 right-0 bg-black text-white py-4 px-6 shadow-lg z-50">
-      <h1 className="text-center text-2xl font-bold">JCSx</h1>
-    </div>
-  );
+    return (
+        <div className="hidden md:flex fixed top-0 left-0 right-0 bg-black text-white py-4 px-6 shadow-lg z-50 justify-center items-center">
+            <img src={logoJcsx} alt="Logo JCSx" className="w-15 h-15" />
+        </div>
+    );
 };
 
 export default Header;
