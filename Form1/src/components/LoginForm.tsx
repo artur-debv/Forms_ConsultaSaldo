f8a94f60e4d168c048c1c0febc4b54363cf10bb4
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-12-12 09:27:27 -0300

    feat: I changed the color of the button in the global variable I created, I added the jcsx logo as a png, I added the invert property to change the logo from white to black, I changed the text accessing your account with the jcsx logo

--- Form1/src/components/LoginForm.tsx
+++ Form1/src/components/LoginForm.tsx
@@ -1,5 +1,6 @@
 import InputField from './InputField';
 import Button from './Button';
+import logoJcsx from '../assets/logoJcsx.svg';
 //import SocialButton from './SocialButton';
 //import { FaGoogle } from "react-icons/fa";
 
@@ -7,7 +8,7 @@ const LoginForm = () => {
     return (
         <div className="flex items-center justify-center min-h-screen">
             <div
-                className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-md"
+                className="w-full max-w-md p-8  space-y-6 bg-white rounded-lg shadow-md"
                 style={{ backgroundColor: 'var(--Background-color)' }} // Aplica a cor da variável
             >
                 <div className="text-center">
@@ -15,8 +16,14 @@ const LoginForm = () => {
                         className="text-2xl font-bold"
                         style={{ color: 'var(--text-color-Form)' }}  // Aplica a cor da variável
                     >
-                        Acesse sua conta
                     </h2>
+                    <img
+                        src={logoJcsx}
+                        alt="Logo Jcsx"
+                        className="mx-auto mb-10"
+                        style={{ filter: 'invert(2)' }}
+                    />
+
                     <p className="text-gray-600"></p>
                 </div>
                 <form className="mt-8 space-y-6">
