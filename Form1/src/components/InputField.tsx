8de41cafcd29497b6846517767fd782d68c5fc5b
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-12-18 11:27:41 -0300

    feat: I added validations to the fields using zod in react

--- Form1/src/components/InputField.tsx
+++ Form1/src/components/InputField.tsx
@@ -1,22 +1,19 @@
-import React from 'react';
+import { forwardRef } from 'react';
 
-interface InputFieldProps {
+// Define os tipos das propriedades do InputField
+type InputFieldProps = {
   type: string;
   placeholder: string;
-}
+} & React.InputHTMLAttributes<HTMLInputElement>;
 
-const InputField: React.FC<InputFieldProps> = ({  type, placeholder }) => {
-  return (
-    <div>
-      <label  className="sr-only">{placeholder}</label>
-      <input
-        type={type}
-        required
-        className="appearance-none mb-4 rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm "
-        placeholder={placeholder}
-      />
-    </div>
-  );
-};
+const InputField = forwardRef<HTMLInputElement, InputFieldProps>(({ type, placeholder, ...rest }, ref) => (
+  <input
+    type={type}
+    placeholder={placeholder}
+    ref={ref}
+    {...rest}
+    className="appearance-none mb-4 rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm "
+  />
+));
 
 export default InputField;
--- Form1/src/components/LoginForm.tsx
+++ Form1/src/components/LoginForm.tsx
@@ -3,21 +3,40 @@ import Button from './Button';
 import logoJcsx from '../assets/logoJcsx.svg';
 //import SocialButton from './SocialButton';
 //import { FaGoogle } from "react-icons/fa";
-import { useForm } from 'react-hook-form'
 import { useState } from 'react';
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const CreateUserFormSchema = z.object({
+    email: z.string()
+        .nonempty("Email é obrigatório")
+        .email('O Formato do e-mail inválido'),
+    password: z.string()
+        .min(6, 'a senha precisa ter no minimo 6 caracteres ')
+});
+
+type CreateUserFormdata = z.infer<typeof CreateUserFormSchema>
 
 const LoginForm = () => {
 
     const [OutPut, setOutPut] = useState('')
 
-    const { register, handleSubmit } = useForm()
+    const {
+        register,
+        handleSubmit,
+        formState: { errors } }
+        = useForm<CreateUserFormdata>({
+            resolver: zodResolver(CreateUserFormSchema),
+        })
 
     function CreateUser(data: any) {
-        setOutPut(JSON.stringify(data, null , 2))
+        setOutPut(JSON.stringify(data, null, 2))
     }
 
     return (
-        <div className="flex items-center justify-center min-h-screen">
+
+        <div className="flex items-center justify-center flex-col min-h-screen">
             <div
                 className="w-full max-w-md p-8  space-y-6 bg-white rounded-lg shadow-md"
                 style={{ backgroundColor: 'var(--Background-color)' }} // Aplica a cor da variável
@@ -40,7 +59,9 @@ const LoginForm = () => {
                 <form onSubmit={handleSubmit(CreateUser)} className="mt-8 space-y-6">
                     <div className="rounded-md shadow-sm -space-y-px">
                         <InputField type="email" placeholder="Email" {...register('email')} />
+                        {errors.email && <span>{errors.email.message}</span>}
                         <InputField type="password" placeholder="Senha"  {...register('password')} />
+                        {errors.password && <span>{errors.password.message}</span>}
                     </div>
                     <div className="flex items-center justify-between">
                         <div className="text-sm">
