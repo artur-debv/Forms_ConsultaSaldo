91f15a490085d5ed69214133b461e171169884dc
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2025-01-22 09:33:25 -0300

    add packagen json

--- Form2/test/Login.test.tsx
+++ Form2/test/Login.test.tsx
@@ -1,3 +1,10 @@
-describe("login",()=>{
-    
+import { render } from "@testing-library/react";
+
+describe("login", () => {
+    it("should render login form", () => {
+        const { getByTestId } = render(<Login />)
+        expect(getByTestId("login-form")).toBeInTheDocument()
+    }
 })
+
+
